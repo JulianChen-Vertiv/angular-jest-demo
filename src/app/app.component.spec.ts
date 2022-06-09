6a41bf2f9d0b07407a43b4bf5f9de6c7f7358e36
Julian Chen <julia.cehn@vertiv.com>
2022-06-10 00:13:15 +0800

    feat: complete all test coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
@@ -11,17 +13,20 @@ import { AuthService } from './services/auth/auth.service';
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         AppModule,
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        RouterModule,
+        RouterTestingModule
       ],
       providers: [
         AuthService,
-        {provide: APP_BASE_HREF, useValue : '/' }
+        { provide: APP_BASE_HREF, useValue : '/' }
       ],
       schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
@@ -30,6 +35,7 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
   });
 
   it('should create the app', () => {
@@ -40,11 +46,38 @@ describe('AppComponent', () => {
     expect(component.title).toEqual('Angular with Jest');
   });
 
-  it('should have two nav links', () => {
+  it('should have one nav links without login', () => {
     fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('a')?.length).toBe(1);
+    expect(compiled.querySelectorAll('a')?.item(0).textContent).toBe('Dashboard');
+  });
+
+
+  it('should have two nav links without login', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+
+    component.login('ROLE_ADMIN');
+    fixture.detectChanges();
+
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelectorAll('a')?.length).toBe(2);
     expect(compiled.querySelectorAll('a')?.item(0).textContent).toBe('Dashboard');
     expect(compiled.querySelectorAll('a')?.item(1).textContent).toBe('Heroes');
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/heroes']);
+  });
+
+  it('should logout successful', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+
+    component.logout();
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('a')?.length).toBe(1);
+    expect(compiled.querySelectorAll('a')?.item(0).textContent).toBe('Dashboard');
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/dashboard']);
   });
 });
